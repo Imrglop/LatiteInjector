5994f6e04cc6c48d7445c5bb93a65ffb22326f95
Imrglop <69129770+Imrglop@users.noreply.github.com>
2023-02-10 14:49:26 -0500

    Revert "Injector now minimizes to tray"
    This reverts commit 6578e45ad1f183b7932855129b58f624f11881cf.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,12 @@
-﻿namespace LatiteInjector;
+﻿using System.Windows;
+using LatiteInjector.Utils;
+
+namespace LatiteInjector;
 
 /// <summary>
 ///     Interaction logic for App.xaml
 /// </summary>
 public partial class App
 {
+    private void App_OnExit(object sender, ExitEventArgs e) => DiscordPresence.StopPresence();
 }
